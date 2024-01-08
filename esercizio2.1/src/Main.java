a8836e55085fb55246ef703fa83d29258136fdbd
Alfredo <alfredcastaldi1992@gmail.com>
2024-01-08 10:38:38 +0100

    esercizio 1

--- esercizio2.1/src/Main.java
+++ esercizio2.1/src/Main.java
@@ -1,2 +1,10 @@
 public class Main {
+    public static void main (String[] args){
+        Studente studente1 = new Studente("mario","rossi");
+        String nome = studente1.getName();
+        String surname = studente1.getSurname();
+        int id = studente1.getId();
+
+        System.out.println(nome + " " + surname + " " + id);
+    }
 }
---
+++ esercizio2.1/src/Studente.java
@@ -0,0 +1,35 @@
+public class Studente {
+    private String name;
+    private String surname;
+    private int id;
+
+    public Studente(String name, String surname){
+        this.name = name;
+        this.surname = surname;
+        this.id++;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
