8941d7e20230d31d7a08be3f111d25a3c32e4ec8
Alfredo <alfredcastaldi1992@gmail.com>
2024-01-09 12:11:32 +0100

    esercizio 3.4

---
+++ esercizio 3.3/src/Forma.java
@@ -0,0 +1,21 @@
+public abstract class Forma {
+    protected double lato;
+    protected double altezza;
+    protected double base;
+
+    public Forma(double lato,double altezza, double base){
+
+        this.lato = lato;
+        this.altezza = altezza;
+        this.base = base;
+    }
+    public Forma(double altezza, double base){
+
+        this.altezza = altezza;
+        this.base = base;
+    }
+
+    public double getArea(){
+        return this.altezza * this.base;
+    }
+}
---
+++ esercizio 3.3/src/Rettangolo.java
@@ -0,0 +1,11 @@
+public class Rettangolo extends Forma{
+
+    public Rettangolo(double base, double lato){
+          super(lato,lato,base);
+    }
+
+
+    public double getArea() {
+        return super.getArea();
+    }
+}
